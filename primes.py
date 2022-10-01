c320e555b33eea50218b0b222c2e60693a6bb8e0
Charlotte Hoi Yi Leong <charlotte.leong@kcl.ac.uk>
2022-10-01 15:34:53 +0100

    first attempt

--- primes.py
+++ primes.py
@@ -3,4 +3,13 @@
 
 def primes(number_of_primes):
     list = []
+    num = 1
+    while len(list) < number_of_primes:
+        flag = True
+        for i in range(1, num):
+            if(num % i ==0):
+                flag = False
+        if(flag):
+            list.append(num)
+        num += 1
     return list
